2a9dafb872aff613e5f4b9c8bbbbd97db81102e0
alekseypermyakov-cmyk <aleksey.permyakov@gmail.com>
2026-05-13 22:05:40 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env sh
+#!/bin/sh
 set -eu
 PORT="${PORT:-10000}"
 sed "s/"port": 10000/"port": ${PORT}/" /app/config.json > /app/config.runtime.json
-exec /usr/bin/xray run -c /app/config.runtime.json
+exec /usr/local/bin/xray run -c /app/config.runtime.json
